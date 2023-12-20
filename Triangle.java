9d0e86853bd695dbe23b14cc0a6566eef01cfc8c
kerenzilberman <154364524+kerenzilberman@users.noreply.github.com>
2023-12-20 21:10:26 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,12 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+         int a = Integer.parseInt(args[0]);
+	 int b = Integer.parseInt(args[1]);
+	 int c = Integer.parseInt(args[2]);
+	 if (a+b>c && b+c>a && a+c>b) 
+	    System.out.println (a + ", " + b + ", " + c + ": true");
+	 else
+	    System.out.println (a + ", " + b + ", " + c + ": false");
 	}
 }
