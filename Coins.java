db612cb5fe6a0c47fcbba07dbf8cda2a32ff59c1
kerenzilberman <kerenamalia.zilberm@post.runi.ac.il>
2023-12-21 19:17:56 +0200

    final

--- Coins.java
+++ Coins.java
@@ -1,4 +1,4 @@
-/*
+/* kerenzilberman
  * Write a program that gets a quantity of cents as a command-line argument.
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
